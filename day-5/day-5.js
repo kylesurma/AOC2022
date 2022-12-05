5273e1be3985d5a38ca2c2d8bbd2c56d889cc467
Kyle Surma <kyle.surma@annalect.com>
2022-12-05 16:05:19 -0500

    cleaned up day 5 & npm init

--- day-5/day-5.js
+++ day-5/day-5.js
@@ -1,25 +1,5 @@
 const moves = require('./all-moves')
-const dock = [
-  ['H', 'T', 'Z', 'D' ],
-  ['Q', 'R', 'W', 'T', 'G', 'C', 'S'],
-  ['P', 'B', 'F', 'Q', 'N', 'R', 'C', 'H'],
-  ['L', 'C', 'N', 'F', 'H', 'Z' ],
-  ['G', 'L', 'F', 'Q', 'S' ],
-  ['V', 'P', 'W', 'Z', 'B', 'R', 'C', 'S'],
-  ['Z', 'F', 'J' ],
-  ['D', 'L', 'V', 'Z', 'R', 'H', 'Q'],
-  ['B', 'H', 'G', 'N', 'F', 'Z', 'L', 'D']
-    ]
-
-movesTest = ['move 1 from 4 to 8',
-'move 2 from 1 to 9',
-'move 4 from 6 to 5',
-'move 1 from 7 to 6',
-'move 2 from 1 to 4',
-'move 7 from 8 to 9',
-'move 7 from 4 to 5',
-'move 4 from 2 to 4',
-'move 1 from 5 to 9',]
+const dock = require('./dock')
 
 const shippingContainerMover  = (commandStr, isOneCrateAtATime = false) => {
     const [howMany, from, to] = commandStr.split(' ').filter(item => +item)
@@ -38,6 +18,8 @@ const runMoves = (moves, isOneCrateAtATime = false) => {
        return runMoves(moves, isOneCrateAtATime)
     }
 }
-
+// grab all crates at the same time
 console.log(runMoves(moves, false))
+// grab one crate at a time
+console.log(runMoves(moves, true))
 
